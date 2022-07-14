d23362db85974790cb7bdef5a83ff22c7aeb9abc
Hoodoo42 <create@hoodooverse.ca>
2022-07-14 19:55:13 -0400

    created queryselector functions to change specific elements in the html

---
+++ js/app.js
@@ -0,0 +1,12 @@
+
+// creating a variable to access the specific id selected by the query
+let welcomeWords = document.querySelector(`#welcome`);
+// using variable and innerText to change the string of the selected element
+welcomeWords[`innerText`] = `Howdy Y'all!`
+
+// creating variable to access all elements that share the same class
+let oneClass = document.querySelectorAll(`.text`);
+// creating a for loop that will help change each elements innerText one by one
+for(let i=0; i<oneClass.length; i++){
+    oneClass[i][`innerText`] = `Grabbed by my class!`
+}
